2e4ee339bbef06ec31615cfbe5828e2e9452d638
mohanddo <93376862+mohanddo@users.noreply.github.com>
2024-09-04 13:37:27 +0100

    Fixed some bugs

--- app/src/main/java/com/example/usthb9raya/AboutUsthb.kt
+++ app/src/main/java/com/example/usthb9raya/AboutUsthb.kt
@@ -16,6 +16,10 @@ class AboutUsthb : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         enableEdgeToEdge()
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
